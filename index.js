076df08c97a250da353e0571fe578aef1268bd2f
Mehwish Aziz <mehwisha@msn.com>
2018-07-22 03:40:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ const newID = 1000000000 + oldID;
 let ageIsValid = Number.isInteger();
 let randomNumber = Math.floor(Math.random() * 20) + 1;
 let randomInteger = Math.round(randomNumber);
-let randomUserID = randomNumber + randomInteger;
+let randomUserID = randomInteger + randomNumber;
